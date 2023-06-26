e5819cd56c83747177380f7811ab5eef9259a35a
Md Zahid Hasan <hasanmd1@fit.cvut.cz>
2023-06-26 14:06:34 +0200

    adding third website

--- website-2/src/containers/Chef/Chef.jsx
+++ website-2/src/containers/Chef/Chef.jsx
@@ -12,19 +12,20 @@ const Chef = () => {
       <div className="app__wrapper_info">
         <SubHeading title="Chef's Word"/>
         <h1 className="headtext__cormorant">What we believe in</h1>
-      </div>
-      <div className="app__chef-content">
-        <div className="app__chef-content_quote">
-          <img src={images.quote} alt="chef_quote" />
-          <p className="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit .</p>
+  
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <img src={images.quote} alt="chef_quote" />
+            <p className="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit .</p>
+          </div>
+          <p className="p__opensans">Auctor Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit. Nulla Scelerisque Scelerisque Congue Ac Consequat, Aliquam Molestie Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.</p>
         </div>
-        <p className="p__opensans">Auctor Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit. Nulla Scelerisque Scelerisque Congue Ac Consequat, Aliquam Molestie Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.</p>
-      </div>
 
-      <div className="app__chef-sign">
-        <p>Kevin Luo</p>
-        <p className="p__opensans">Chef & Founder</p>
-        <img src={images.sign} alt="chef_sign" />
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="chef_sign" />
+        </div>
       </div>
     </div>
   )
